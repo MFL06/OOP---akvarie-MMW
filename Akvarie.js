2646ee53b715889357a9366dc8660c1ab169cc54
MFL <mikkelljungbeck@gmail.com>
2025-03-26 10:16:55 +0100

    Bundlinjen

---
+++ Akvarie.js
@@ -0,0 +1,58 @@
+function setup(){
+    createCanvas(400,400);
+    TenBoxes();
+}
+
+function draw(){
+    background('white');
+    ShowBoxes();
+}
+
+class Box{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.w = 25;
+        this.h = 25;
+        this.c = 'red';
+    }
+
+
+
+    show(){
+        stroke(this.c)
+        fill(this.c)
+        rect(this.x, this.y,this.w, this.h);
+    }
+
+
+    VibrateIn(){
+
+    }
+
+    VibrateOut(){
+
+    }
+
+}
+
+function TenBoxes(){
+    arr = [];
+    for(let i = 0; i != 10; i++){
+        let x = random(0, 201);
+        let y = random(0, 201);
+        arr[i] = new Box(x, y);
+    }
+}
+
+function ShowBoxes(){
+    for(let i = 0; i != 10; i++){
+        arr[i].show();
+    }
+}
+
+let xVib, yVib;
+
+let arr;
+
+let b1, b2;
