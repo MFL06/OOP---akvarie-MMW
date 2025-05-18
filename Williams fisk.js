667d1a90f3715ac440b80611d39f667f83c2dc6a
H3rsk <will.herskind@gmail.com>
2025-05-18 19:13:54 +0200

    Færdig fisk

--- Williams fisk.js
+++ Williams fisk.js
@@ -1,56 +1,120 @@
-let fisk=[]
-
+let fisk = [];
 
 function setup() {
-    createCanvas(400, 400);
-    lavfisk()
-    
-  }
-  
-  function draw() {
-    background(220);
-    for (let i = 0; i < fisk.length; i++) {
-        fisk[i].show();
-    }
-  }
-  
+  createCanvas(400, 400);
+  lavfisk();
+}
 
-  function lavfisk(){
-    for (let i = 0; i < 30; i++) {
-        fisk.push(new Box(random(0,400),random(0,400)))
-      }
+function draw() {
+  background(220);
+  for (let f of fisk) {
+    f.wander();
+    f.avoidWalls();
+    f.update();
+    f.show();  // Polymorfi – hver fisketype har sin egen show-metode
+  }
+}
+
+function lavfisk() {
+  for (let i = 0; i < 15; i++) {
+    fisk.push(new Box(random(width), random(height))); // Box-fisk
+    fisk.push(new CircleFish(random(width), random(height))); // Cirkel-fisk
+  }
+}
+
+// Base klasse
+class Fish {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.direction = new Vector(random(-1, 1), random(-1, 1));
+    this.direction.normalize();
   }
 
-  class Vector{
-    constructor(x,y){
-      this.x=x
-      this.y=y
-    }
-    normalize(){
-      let length = this.length()
-      this.x=this.x/length
-      this.y=this.y/length
-    }
-    length(){
-      return sqrt(this.x**2+this.y**2)
-    }
+  wander() {
+    let maxAngle = 0.1;
+    this.direction.rotate(random(-maxAngle, maxAngle));
+    this.direction.normalize();
   }
-  
-  class Box{
-    constructor(x,y){
-    this.x=x
-    this.y=y
-    this.w=1
-    this.h=1
-    this.col="red"
-    this.direction=new Vector(1,1)
-    }
 
-    show(){
-      this.direction.normalize()
-      this.x+=this.direction.x
-      this.y+=this.direction.y
-      fill(this.col)
-      rect(this.x,this.y,this.w,this.h)
+  avoidWalls() {
+    let margin = 20;
+    let turnAngle = 0.2;
+    if (this.x < margin) this.direction.rotate(turnAngle);
+    else if (this.x > width - margin) this.direction.rotate(-turnAngle);
+    if (this.y < margin) this.direction.rotate(turnAngle);
+    else if (this.y > height - margin) this.direction.rotate(-turnAngle);
+    this.direction.normalize();
+  }
+
+  update() {
+    this.x += this.direction.x;
+    this.y += this.direction.y;
+    this.x = constrain(this.x, 0, width);
+    this.y = constrain(this.y, 0, height);
+  }
+
+  show() {
+    // Skal overskrives af subklasser
+  }
+}
+
+// Box-fisk
+class Box extends Fish {
+  constructor(x, y) {
+    super(x, y);
+    this.w = 4;
+    this.h = 4;
+    this.col = color(255, 100, 100);
+  }
+
+  show() {
+    noStroke();
+    fill(this.col);
+    rect(this.x, this.y, this.w, this.h);
+  }
+}
+
+// Cirkel-fisk
+class CircleFish extends Fish {
+  constructor(x, y) {
+    super(x, y);
+    this.r = 5;
+    this.col = color(100, 100, 255);
+  }
+
+  show() {
+    noStroke();
+    fill(this.col);
+    ellipse(this.x, this.y, this.r * 2);
+  }
+}
+
+// Vektor-klasse
+class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  normalize() {
+    let len = this.length();
+    if (len) {
+      this.x /= len;
+      this.y /= len;
     }
   }
+
+  length() {
+    return sqrt(this.x * this.x + this.y * this.y);
+  }
+
+  rotate(angle) {
+    let c = cos(angle),
+        s = sin(angle),
+        nx = this.x * c - this.y * s,
+        ny = this.x * s + this.y * c;
+    this.x = nx;
+    this.y = ny;
+  }
+}
